0e827b0b1a4f0539bdc110eac1f2f60cbdf06c9d
ayush <99374392+ayush-os@users.noreply.github.com>
2025-12-04 09:07:53 -0800

    change formula

--- src/kernels/kernel_baseline.cu
+++ src/kernels/kernel_baseline.cu
@@ -10,5 +10,10 @@ __global__ void baseline_elementwise_kernel(float* output, const float* input, i
 
     float in = input[threadId];
 
-    output[threadId] = in * __nv_normcdf(in);
+    float sqrt2_recip = 0.70710678118f; // 1.0f / sqrt(2.0f)
+    float term = in * sqrt2_recip; 
+
+    // GELU(x) = x * Phi(x)
+    // Phi(x) = 0.5 * (1 + erff(x / sqrt(2)))
+    output[threadId] = in * 0.5f * (1.0f + erff(term));
 }
