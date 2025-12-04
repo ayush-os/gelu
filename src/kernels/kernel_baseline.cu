6ebd7d4489befc7e6a401b2aac2a13f980eb9ca4
ayush <99374392+ayush-os@users.noreply.github.com>
2025-12-04 08:48:16 -0800

    baseline kernel

--- src/kernels/kernel_baseline.cu
+++ src/kernels/kernel_baseline.cu
@@ -2,4 +2,12 @@
 // --- Baseline Kernel (Naive, Unoptimized) ---
 // This uses a direct 1-to-1 mapping from thread index to array index.
 
-__global__ void baseline_elementwise_kernel(float* output, const float* input, int N) {}
+__global__ void baseline_elementwise_kernel(float* output, const float* input, int N) {
+    const int threadId = blockIdx.x * blockDim.x + threadIdx.x;
+
+    if (threadId >= N) return;
+
+    float in = input[threadId];
+
+    output[threadId] = in * __nv_normcdf(in);
+}
